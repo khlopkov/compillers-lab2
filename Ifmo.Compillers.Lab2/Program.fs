ee465b1d4d67949ffb47a084c53fcd2907facb51
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-05-06 18:46:44 +0300

    Test functionality

--- Ifmo.Compillers.Lab2/Program.fs
+++ Ifmo.Compillers.Lab2/Program.fs
@@ -1,8 +1,16 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Ifmo.Compillers.LexicalAnalyzer
+open Ifmo.Compillers.LexicalAnalyzer.Tokens
+
+let tokenListToString (list: list<Token>) = 
+    let tokenToStr (t: Token) = t.ToString
+    List.map tokenToStr list
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let tokens = Analyzer.analyze "Begin End. Var kek"
+    printfn "%A" <| tokenListToString tokens
+    Console.ReadKey()
     0 // return an integer exit code
